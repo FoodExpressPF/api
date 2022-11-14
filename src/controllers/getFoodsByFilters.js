5f9aca8ea478a7121b945ae4d7e128aad45b869e
carlos1-1 <carlosfrangomez@gmail.com>
2022-11-14 13:42:23 -0300

    modificacion del filtro por id, solucionando un error donde no mandaba a la ruta especificada

--- src/controllers/getFoodsByFilters.js
+++ src/controllers/getFoodsByFilters.js
@@ -1,10 +1,10 @@
-const { StatusCodes } = require('http-status-codes');
+const { StatusCodes } = require("http-status-codes");
 const { Foods } = require("../db");
 const callApi = require("../utils/callApi.js");
 
 const getAllFoods = async () => {
   // temporal function
-  try{
+  try {
     const foodApi = await callApi();
     const foodDb = await Foods.findAll();
     const allFoods = [...foodDb, ...foodApi];
@@ -13,71 +13,62 @@ const getAllFoods = async () => {
     throw {
       status: error.status || StatusCodes.BAD_REQUEST,
       reason: error.reason || error,
-    }
+    };
   }
-}
+};
 
-const getFoodsByFilters = async ({
-  id,
-  name,
-  type,
-  offer,
-  sortby,
-  asc,
-}) => {
-  
+const getFoodsByFilters = async ({ id, name, type, offer, sortby, asc }) => {
   try {
     const allFoods = await getAllFoods();
     //const allFoods = await Foods.findAll();
     let filteredFoods = [...allFoods];
-    if(!!id) {
-      const foundFood = allFoods.find(food => food.id === parseInt(id));
+    if (!!id) {
+      const foundFood = allFoods.find((food) => food.id == id);
       return [foundFood];
     }
-    
-    if(!!name) filteredFoods = filteredFoods.filter(food => {
-      const foodName = food.name.toUpperCase();
-      return foodName.includes(name.toUpperCase());
-    });
 
-    if(!!type) filteredFoods = filteredFoods.filter(food => {
-      return food.type.includes(type);
-    });
-    
+    if (!!name)
+      filteredFoods = filteredFoods.filter((food) => {
+        const foodName = food.name.toUpperCase();
+        return foodName.includes(name.toUpperCase());
+      });
+
+    if (!!type)
+      filteredFoods = filteredFoods.filter((food) => {
+        return food.type.includes(type);
+      });
+
     switch (offer) {
       case "yes":
-        filteredFoods = filteredFoods.filter(food => food.offer);
+        filteredFoods = filteredFoods.filter((food) => food.offer);
         break;
       case "no":
-        filteredFoods = filteredFoods.filter(food => !food.offer);
+        filteredFoods = filteredFoods.filter((food) => !food.offer);
       // case default: all
     }
 
     switch (sortby) {
       case "price":
-        filteredFoods = filteredFoods.sort(
-          a.price - b.price
-        ); break;
+        filteredFoods = filteredFoods.sort(a.price - b.price);
+        break;
       case "rating":
-        filteredFoods = filteredFoods.sort((a,b) =>
-          a.rating - b.rating
-        ); break;
-      default:   // name
-        filteredFoods = filteredFoods.sort((a,b) => 
+        filteredFoods = filteredFoods.sort((a, b) => a.rating - b.rating);
+        break;
+      default: // name
+        filteredFoods = filteredFoods.sort((a, b) =>
           a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1
         );
     }
-    
-    if(!asc) filteredFoods = filteredFoods.reverse();
+
+    if (!asc) filteredFoods = filteredFoods.reverse();
 
     return filteredFoods;
-
   } catch (error) {
     throw {
       status: error.status || StatusCodes.BAD_REQUEST,
       reason: error.reason || error,
-    }
+    };
   }
-}
+};
 
 module.exports = getFoodsByFilters;
