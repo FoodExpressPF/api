6aa7532359721c334842ffed3345f1fada8dc4c1
carlos1-1 <carlosfrangomez@gmail.com>
2022-11-16 23:18:38 -0300

    implementacion de la pasarela de pagos(mercado pago) y el envio automatico de mails a las cuentas de usuarios, momentaneamente los correos llegan desde una cuenta mia puesto que espero confirmacion de todos para hacer la modificacion requerida en la cuenta compartida de foodExpres

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,12 @@ const getFavorites = require("./favorites/get.js");
 const putFavorites = require("./favorites/put.js");
 const deleteFavorites = require("./favorites/delete.js");
 
+const PaymentController = require("../controllers/PaymentController");
+const PaymentService = require("../utils/PaymentService");
+const PaymentInstance = new PaymentController(new PaymentService());
+
+const nodemailer = require("nodemailer");
+
 router.use("/foods", getFoods);
 router.use("/foods/create", postFoods);
 router.use("/user/create", postUser);
@@ -19,4 +25,35 @@ router.use("/user/update", updateUser);
 router.use("/favorites/get", getFavorites);
 router.use("/favorites/put", putFavorites);
 router.use("/favorites/delete", deleteFavorites);
+
+router.post("/payment", function (req, res, next) {
+  PaymentInstance.getPaymentLink(req, res); //recibir items del front, al controller y finalmente al utils
+});
+
+router.post("/send-email", (req, res) => {
+  const { user, subject, text } = req.body;
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    post: 587,
+    auth: {
+      user: "cheshiree72@gmail.com",
+      pass: "ioixgkmvtgpqvkko",
+    },
+  }); //por req debera llegar  un objeto con user.email e info del mensaje
+  let mailOptions = {
+    from: "Remitente",
+    to: user,
+    subject: subject,
+    text: text,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send(error.message);
+    } else {
+      console.log("enviado");
+      res.status(200).json(req.body);
+    }
+  });
+});
+
 module.exports = router;
---
+++ src/utils/PaymentService.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+class PaymentService {
+  async createPayment(total) {
+    const url = "https://api.mercadopago.com/checkout/preferences";
+
+    const body = {
+      payer_email: "test_user_46945293@testuser.com",
+
+      items: [
+        {
+          title: "total",
+          description: "de aca pa google",
+          picture_url: "http://www.myapp.com/myimage.jpg",
+          category_id: "category123",
+          quantity: 1,
+          unit_price: total,
+        },
+      ],
+      back_urls: {
+        failure: "/failure",
+        pending: "/pending",
+        success: "/succes",
+      },
+    };
+
+    const payment = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
+    });
+
+    return payment.data;
+  }
+}
+
+module.exports = PaymentService;
