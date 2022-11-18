159f52c03d83b02d4d41e90d3e44da08715c00b2
julian-ncanziani <julian.noya@hotmail.com>
2022-11-18 01:01:27 -0300

    cambios en model user/ rutas update user

--- src/routes/user/update.js
+++ src/routes/user/update.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { User } = require('../../db');
 const { ReasonPhrases, StatusCodes } = require("http-status-codes");
 
-router.put("/", async(req, res)=>{
-    const {action} = req.query;
-    const {id} = req.body;
+router.put("/:action", async(req, res)=>{
+    const {action} = req.params;
+    const {id, name, direction, number_phone} = req.body;
     let update;
     try {
         if(action == "delete"){
@@ -12,6 +12,9 @@ router.put("/", async(req, res)=>{
         }
         if(action == "activate"){
             update = await User.update({visible: true}, {where: {visible: false, id: id}});
+        }
+        if(action == "changefields"){
+            update = await User.update({name: name, direction: direction, number_phone: number_phone}, {where: {id: id}});
         }
         if(update[0] == 0) throw({status: StatusCodes.NOT_MODIFIED, reason: ReasonPhrases.NOT_MODIFIED});
         return res.status(StatusCodes.OK).json({message: "Updated user status"});
