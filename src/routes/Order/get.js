264957285538d076116a29860e103af166c80b2b
carlos1-1 <carlosfrangomez@gmail.com>
2022-11-21 22:56:03 -0300

    .

--- src/routes/Order/get.js
+++ src/routes/Order/get.js
@@ -1,15 +1,33 @@
 const router = require("express").Router();
-const { Order } = require("../../db");
+const { Order, User, Foods_Order } = require("../../db");
 const { ReasonPhrases, StatusCodes } = require("http-status-codes");
 
 router.get("/", async (req, res) => {
   let orders;
+
   try {
     orders = await Order.findAll();
+
     res.status(200).json(orders);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
 });
 
+/*router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const order_user = await Foods_Order.findAll({
+      where: { userId: id },
+      include: Order,
+    });
+    return res.status(200).res("order");
+  } catch (error) {
+    return res
+      .status(error.status || StatusCodes.BAD_REQUEST)
+      .send(error.reason || error);
+  }
+});
+*/
 module.exports = router;
