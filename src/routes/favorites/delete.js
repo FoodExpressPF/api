2969a3fba4c39f1b523d275bd6d553f7fbc9874a
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-11-28 13:46:42 -0300

    cambios menores en favorites

--- src/routes/favorites/delete.js
+++ src/routes/favorites/delete.js
@@ -1,16 +1,22 @@
 const router = require("express").Router();
 const { Favorites } = require('../../db');
-const { ReasonPhrases, StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 
 router.delete("/", async(req, res)=>{
-    const {foodId} = req.body;
+  const { foodId } = req.body;
 
-    try {
-        const deletedFoodFromFavorites = await Favorites.destroy({where: {foodId: foodId}});
-        res.status(202).json(deletedFoodFromFavorites); 
-    } catch (error) {
-        res.status(400).json(error);
-    }
+  try {
+    const deletedFoodFromFavorites = await Favorites.destroy({where: { foodId }});
+    return res
+      .status(StatusCodes.OK)
+      .json(deletedFoodFromFavorites)
+    ;
+  } catch (error) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(error)
+    ;
+  }
 });
 
 module.exports = router;
--- src/routes/favorites/get.js
+++ src/routes/favorites/get.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
-const { Favorites, Foods, User} = require('../../db');
-const { ReasonPhrases, StatusCodes } = require("http-status-codes");
-
-
+const { Foods, User} = require('../../db');
+const { StatusCodes } = require("http-status-codes");
 
 router.get("/", async(req, res)=>{
-    const {userId} = req.body;
-    
-    try {
-        const favorites = await User.findOne({where : {id: userId},include: Foods});
-        res.json(favorites.foods);
-    } catch (error) {
-        res.json(error);
-    }
+  const { userId } = req.body;
+  
+  try {
+    const favorites = await User.findOne({ where: {id: userId}, include: Foods });
+    return res
+      .status(StatusCodes.OK)
+      .json(favorites.foods)
+    ;
+  } catch (error) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json(error)
+      ;
+  }
 });
 
 module.exports = router;
